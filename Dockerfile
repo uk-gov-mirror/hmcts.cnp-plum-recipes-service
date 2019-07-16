41511ed49ff094ce44a448bab8666e3ff2dd6d68
Tim Jacomb <tim.jacomb@hmcts.net>
2019-07-16 10:08:31 +0100

    Use appinsights that works on java 11 (#54)
    * Use appinsights that works on java 11
    
    * Fix dep check issues
    
    * Fix helm chart

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,8 @@
-FROM hmcts/cnp-java-base:openjdk-11-distroless-1.0-beta
+ARG APP_INSIGHTS_AGENT_VERSION=2.5.0-BETA
 
-COPY lib/applicationinsights-agent-2.4.0-BETA-SNAPSHOT.jar lib/AI-Agent.xml /opt/app/
+FROM hmctspublic.azurecr.io/base/java:openjdk-11-distroless-1.0
+
+COPY lib/applicationinsights-agent-2.5.0-BETA.jar lib/AI-Agent.xml /opt/app/
 COPY build/libs/moj-rhubarb-recipes-service.jar /opt/app/
 
 CMD ["moj-rhubarb-recipes-service.jar"]
--- src/main/java/uk/gov/hmcts/reform/rhubarb/recipes/Application.java
+++ src/main/java/uk/gov/hmcts/reform/rhubarb/recipes/Application.java
@@ -2,12 +2,8 @@ package uk.gov.hmcts.reform.rhubarb.recipes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
-@EnableCircuitBreaker
-@EnableHystrixDashboard
 @SuppressWarnings("HideUtilityClassConstructor")
 public class Application {
 
