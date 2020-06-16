e526353f43b08154d5a380c0b6961e0bbcc8b2d1
Piyush Kumar <piyush.m82@gmail.com>
2020-06-16 09:39:41 +0100

    U878 plum api tf (#143)
    Arm to Terraform

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -67,31 +67,34 @@ module "recipe-database" {
   sku_tier           = "GeneralPurpose"
   storage_mb         = "51200"
   common_tags        = var.common_tags
-  subscription = var.subscription
+  subscription       = var.subscription
 }
 
 # region API (gateway)
-
-data "template_file" "api_template" {
-  template = file("${path.module}/template/api.json")
+module "api" {
+  source        = "github.com/hmcts/cnp-module-api-mgmt-api?ref=master"
+  name          = "${var.product}-recipes-api"
+  api_mgmt_rg   = "core-infra-${var.env}"
+  api_mgmt_name = "core-api-mgmt-${var.env}"
+  display_name  = "${var.product}-recipes"
+  path          = local.api_base_path
+  service_url   = "http://${var.product}-${local.app}-${var.env}.service.core-compute-${var.env}.internal"
 }
 
-resource "azurerm_template_deployment" "api" {
-  template_body       = data.template_file.api_template.rendered
-  name                = "${var.product}-api-${var.env}"
-  deployment_mode     = "Incremental"
-  resource_group_name = "core-infra-${var.env}"
-  count               = local.create_api ? 1 : 0
+data "template_file" "plum_api_policy_template" {
+  template = file("${path.module}/templates/api-policy.xml")
 
-  parameters = {
-    apiManagementServiceName = "core-api-mgmt-${var.env}"
-    apiName                  = "${var.product}-recipes-api"
-    apiProductName           = "${var.product}-recipes"
-    serviceUrl               = "http://${var.product}-${local.app}-${var.env}.service.core-compute-${var.env}.internal"
-    apiBasePath              = local.api_base_path
-    policy                   = local.api_policy
+  vars = {
+    allowed_certificate_thumbprints = local.thumbprints_in_quotes_str
   }
 }
 
+module "policy" {
+  source                 = "github.com/hmcts/cnp-module-api-mgmt-api-policy?ref=master"
+  api_mgmt_name          = "core-api-mgmt-${var.env}"
+  api_mgmt_rg            = "core-infra-${var.env}"
+  api_name               = module.api.name
+  api_policy_xml_content = data.template_file.plum_api_policy_template.rendered
+}
 # endregion
 
