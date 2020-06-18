28e7d20ba830b30f3ede0b1c49fa3f32bc2ddce8
Praveen Adusumilli <47391951+adusumillipraveen@users.noreply.github.com>
2020-06-18 10:33:12 +0100

    remove unused property (#150)

--- src/main/java/uk/gov/hmcts/reform/rhubarb/recipes/endpoint/RecipeController.java
+++ src/main/java/uk/gov/hmcts/reform/rhubarb/recipes/endpoint/RecipeController.java
@@ -7,7 +7,6 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import uk.gov.hmcts.reform.rhubarb.recipes.data.RecipeStore;
 import uk.gov.hmcts.reform.rhubarb.recipes.domain.ErrorResult;
@@ -16,7 +15,6 @@ import uk.gov.hmcts.reform.rhubarb.recipes.domain.RecipeList;
 import uk.gov.hmcts.reform.rhubarb.recipes.exception.NoRecipeFoundException;
 
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping(
@@ -51,9 +49,7 @@ public class RecipeController {
     @ApiResponses({
         @ApiResponse(code = 200, message = "Success"),
     })
-    public RecipeList readAll(
-        @RequestParam(required = false) Map<String, String> params
-    ) {
+    public RecipeList readAll() {
 
         List<Recipe> recipes = recipeStore.readAll();
 
