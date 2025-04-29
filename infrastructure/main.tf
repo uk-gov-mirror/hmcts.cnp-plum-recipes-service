7cbdaee48927661489a80615df17eca368aba3ca
anne-moore <Anne.Moore@justice.gov.uk>
2025-04-29 10:41:33 +0100

    Testing postgres module changes

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -74,7 +74,7 @@ module "postgresql_flexible" {
     azurerm.postgres_network = azurerm.postgres_network
   }
 
-  source        = "git@github.com:hmcts/terraform-module-postgresql-flexible?ref=master"
+  source        = var.pgsql_module_version
   env           = var.env
   product       = var.product
   name          = "${var.product}-v14-flexible"
