23a048df3f34c85a498ffbb20b032cabbb5313e4
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2025-07-09 13:55:12 +0100

    fix errors in gradle

--- src/main/java/uk/gov/hmcts/reform/rhubarb/recipes/endpoint/EndpointExceptionHandler.java
+++ src/main/java/uk/gov/hmcts/reform/rhubarb/recipes/endpoint/EndpointExceptionHandler.java
@@ -1,10 +1,10 @@
 package uk.gov.hmcts.reform.rhubarb.recipes.endpoint;
 
-import jakarta.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatusCode;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.ServletRequestBindingException;
@@ -35,7 +35,7 @@ public class EndpointExceptionHandler extends ResponseEntityExceptionHandler {
     protected ResponseEntity<Object> handleServletRequestBindingException(
         ServletRequestBindingException ex,
         HttpHeaders headers,
-        HttpStatusCode status,
+        HttpStatus status,
         WebRequest request
     ) {
         LOGGER.error(ex.getMessage(), ex);
@@ -50,7 +50,7 @@ public class EndpointExceptionHandler extends ResponseEntityExceptionHandler {
     protected ResponseEntity<Object> handleHttpMessageNotReadable(
         HttpMessageNotReadableException ex,
         HttpHeaders headers,
-        HttpStatusCode status,
+        HttpStatus status,
         WebRequest request
     ) {
         LOGGER.error(ex.getMessage(), ex);
@@ -66,7 +66,7 @@ public class EndpointExceptionHandler extends ResponseEntityExceptionHandler {
         Exception ex,
         Object body,
         HttpHeaders headers,
-        HttpStatusCode status,
+        HttpStatus status,
         WebRequest request
     ) {
         LOGGER.error(ex.getMessage(), ex);
