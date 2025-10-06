3258032e0ac26de647e9a2bd47b163eee5f19bf2
Nicholas Chang <60232417+NickAzureDevops@users.noreply.github.com>
2025-10-06 14:23:09 +0100

    DTSPO 278838 SKU to premium (#1236)
    * set default to P temporary
    
    * uncomment default for family
    
    * update to standard
    
    * comment out state and varaible
    
    * update to Premium
    
    * update to Premium
    
    * update to Premium
    
    * update to Premium
    
    * update to Premium
    
    * update to Premium
    
    * update to Premium
    
    * update to Premium
    
    * fix typo
    
    * set to standard
    
    * add capcity
    
    * keep old version
    
    * fix azure version
    
    * update the version
    
    * move the variables from main to variables file
    
    * removed
    
    ---------
    
    Co-authored-by: salim abdulkareem <salim.abdulkareem@justice.gov.uk>

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -100,38 +100,6 @@ module "postgresql_flexible" {
 
 # endregion
 
-# REDIS CACHE TESTING
-
-variable "rdb_backup_enabled" {
-  type    = bool
-  default = false
-}
-
-variable "sku_name" {
-  default     = "Basic"
-  description = "The SKU of Redis to use. Possible values are `Basic`, `Standard` and `Premium`."
-}
-
-variable "family" {
-  default     = "C"
-  description = "The SKU family/pricing group to use. Valid values are `C` (for Basic/Standard SKU family) and `P` (for Premium). Use P for higher availability, but beware it costs a lot more."
-}
-
-variable "redis_capacity" {
-  default     = "1"
-  description = "The size of the Redis cache to deploy. Valid values are 1, 2, 3, 4, 5"
-}
-
-variable "redis_backup_frequency" {
-  default     = "360"
-  description = "The Backup Frequency in Minutes. Only supported on Premium SKUs. Possible values are: 15, 30, 60, 360, 720 and 1440"
-}
-
-variable "rdb_backup_max_snapshot_count" {
-  type    = string
-  default = "1"
-}
-
 module "plum-redis-storage" {
   source                          = "git@github.com:hmcts/cnp-module-redis?ref=DTSPO-17012-data-persistency-4.x"
   product                         = "${var.product}-${var.component}-session-storage"
