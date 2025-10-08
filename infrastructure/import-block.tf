2274a112a09b735fff28248ed38c8743daf13872
salim abdulkareem <salim.abdulkareem@justice.gov.uk>
2025-10-08 19:26:47 +0100

    add import black for redis

---
+++ infrastructure/import-block.tf
@@ -0,0 +1,9 @@
+# Import blocks for existing Redis resources
+
+# Import the existing Redis cache
+import {
+  to = module.plum-redis-storage.azurerm_redis_cache.redis
+  id = "/subscriptions/bf308a5c-0624-4334-8ff8-8dca9fd43783/resourceGroups/plum-recipe-backend-session-storage-cache-sandbox/providers/Microsoft.Cache/Redis/plum-recipe-backend-session-storage-cache-sandbox"
+}
+
+
