615ec8cef86050020708fdf827754739420d6c4f
salim abdulkareem <salim.abdulkareem@justice.gov.uk>
2025-10-08 19:42:18 +0100

    fix the import name

--- infrastructure/import-block.tf
+++ infrastructure/import-block.tf
@@ -3,7 +3,7 @@
 # Import the existing Redis cache
 import {
   to = module.plum-redis-storage.azurerm_redis_cache.redis
-  id = "/subscriptions/bf308a5c-0624-4334-8ff8-8dca9fd43783/resourceGroups/plum-recipe-backend-session-storage-cache-sandbox/providers/Microsoft.Cache/redis/plum-recipe-backend-session-storage-cache-sandbox"
+  id = "/subscriptions/bf308a5c-0624-4334-8ff8-8dca9fd43783/resourceGroups/plum-recipe-backend-session-storage-cache-sandbox/providers/Microsoft.Cache/redis/plum-recipe-backend-session-storage-sandbox"
 }
 
 
