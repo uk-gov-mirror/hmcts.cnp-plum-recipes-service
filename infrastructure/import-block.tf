17a654c24df1034aeac1939e63612674a7d460ec
salim abdulkareem <salim.abdulkareem@justice.gov.uk>
2025-10-09 09:08:35 +0100

    add import block for private endpoint

--- infrastructure/import-block.tf
+++ infrastructure/import-block.tf
@@ -6,4 +6,9 @@ import {
   id = "/subscriptions/bf308a5c-0624-4334-8ff8-8dca9fd43783/resourceGroups/plum-recipe-backend-session-storage-cache-sandbox/providers/Microsoft.Cache/redis/plum-recipe-backend-session-storage-sandbox"
 }
 
+import {
+  to = module.plum-redis-storage.azurerm_private_endpoint.this[0]
+  id = "/subscriptions/bf308a5c-0624-4334-8ff8-8dca9fd43783/resourceGroups/plum-recipe-backend-session-storage-cache-sandbox/providers/Microsoft.Network/privateEndpoints/plum-recipe-backend-session-storage-sandbox"
+}
+
 
